c388e766cac75b18c9c3f705aba0eec8540cc0f8
Aidan O'Connor <aidan.oconnor@snyk.io>
2022-12-13 13:25:30 -0500

    Adding revised main.go, go.mod, go.sum

--- main.go
+++ main.go
@@ -1,29 +1,100 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	// set the router as the default one shipped with Gin
-	router := gin.Default()
+//define structure for sec api response
+type Company struct {
+	Name         string
+	Ticker       string
+	CIK          string
+	CUSIP        string
+	Exchange     string
+	IsDelisted   bool
+	Category     string
+	Sector       string
+	Industry     string
+	Sic          string
+	SicSector    string
+	SicIndustry  string
+	FamaSector   string
+	FamaIndustry string
+	Currency     string
+	Location     string
+	Id           string
+}
 
-	// serve frontend static files
-	router.Use(static.Serve("/", static.LocalFile("./views/js", true)))
+//get the SEC_API_TOKEN from .env file
+func goDotEnvVariable(key string) string {
+	//load .env file
+	err := godotenv.Load(".env")
 
-	// setup route group for the API
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
+	if err != nil {
+		log.Fatalf("Error loading .env file")
 	}
 
-	// start and run the server
-	router.Run(":3000")
+	return os.Getenv(key)
+}
+
+/*
+	Accepts user input about a desired company,
+	checks user input for length,
+	calls sec api to get information about company,
+	returns ticker symbol and location of company
+*/
+func main() {
+
+	//request user input about company
+	fmt.Println("Which company do you want to know about?")
+
+	//set user selected company as string variable
+	var companySelection string
+	fmt.Scanln(&companySelection)
+
+	//set length of companySelection to variable companySelection
+	var companySelectionLength = len(companySelection)
+
+	//check if companySelection is between 4 and 25 characters
+	if companySelectionLength >= 4 && companySelectionLength <= 25 {
+		fmt.Print("\nOkay, you want to know about ", companySelection, "\n")
+
+		//get the SEC_API_TOKEN from .env file to use in api call
+		dotenv := goDotEnvVariable("SEC_API_TOKEN")
+
+		//call the sec api to get information about companySelection
+		res, err := http.Get("https://api.sec-api.io/mapping/name/" + companySelection + "?token=" + dotenv)
+
+		//handle the errors
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var c []Company
+
+		unmarshaledBody := json.Unmarshal(body, &c)
+		if unmarshaledBody != nil {
+			fmt.Println(unmarshaledBody)
+			return
+		}
+
+		for _, values := range c {
+			fmt.Printf("\nTicker: %#v\nLocation: %#v\n", values.Ticker, values.Location)
+		}
+	} else {
+		fmt.Println("Your entry must be between 4 and 25 characters long, please try again")
+	}
 }
