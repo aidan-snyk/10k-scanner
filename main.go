c5e025d53210d7ec1795f9fca39930aa0a3dfd69
Aidan O'Connor <aidan.oconnor@snyk.io>
2022-12-13 22:35:21 -0500

    updated user input as separate function

--- main.go
+++ main.go
@@ -34,22 +34,94 @@ type Company struct {
 
 //get the SEC_API_TOKEN from .env file
 func goDotEnvVariable(key string) string {
+
 	//load .env file
 	err := godotenv.Load(".env")
 
+	//checks for existence and use of .env file
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
+	//return the value of the key in question
 	return os.Getenv(key)
 }
 
-/*
-	Accepts user input about a desired company,
-	checks user input for length,
-	calls sec api to get information about company,
-	returns ticker symbol and location of company
-*/
+//Calls sec api, returns ticker symbol and location of company
+func mappingApiNameCaller(userNameInput string) string {
+
+	//set length of companySelection to variable companySelection
+	var companySelectionLength = len(userNameInput)
+
+	//check if companySelection is between 4 and 25 characters
+	if companySelectionLength >= 4 && companySelectionLength <= 25 {
+
+		fmt.Print("\nOkay, you want to know about ", userNameInput, "\n")
+
+		//get the SEC_API_TOKEN from .env file to use in api call
+		dotenv := goDotEnvVariable("SEC_API_TOKEN")
+
+		//call the sec api to get information about companySelection
+		res, err := http.Get("https://api.sec-api.io/mapping/name/" + userNameInput + "?token=" + dotenv)
+
+		//check api response and present any errors
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		//read the api response and present any errors
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//set variable to iterate through response
+		var c []Company
+
+		//unmarshal the json response of the body
+		unmarshaledBody := json.Unmarshal(body, &c)
+		if unmarshaledBody != nil {
+			fmt.Println(unmarshaledBody)
+		}
+
+		//add logic to execute different behavior for 1 vs many results
+		//if number of results is 0, stop the loop
+		if len(c) == 0 {
+			fmt.Print("\nHmmmm not finding a company that matches that description.\n")
+
+			//if number of results is 1, present only that one
+		} else if len(c) == 1 {
+			fmt.Print("\nNice! Found only one company matching that description:\n")
+			for _, values := range c {
+				if len(values.Ticker) == 0 {
+					fmt.Printf("\nLooks like %#v is not a public company (yet). Sorry!", userNameInput)
+				} else {
+					fmt.Printf("\nTicker: %#v\nLocation: %#v\n", values.Ticker, values.Location)
+				}
+			}
+
+			//if number of results is more than 1, break the loop
+		} else {
+			fmt.Printf("\nLooks like there are %d results, we'll need to narrow this down:\n", len(c))
+			for _, values := range c {
+				if len(values.Ticker) == 0 || values.Ticker == "N/A" {
+					continue
+				} else {
+					fmt.Printf("\nTicker: %#v\nLocation: %#v\n", values.Ticker, values.Location)
+				}
+			}
+		}
+		//reject user input that is not between 4 and 25 characters (inclusive)
+	} else {
+		fmt.Println("Your entry must be between 4 and 25 characters long, please try again")
+	}
+
+	//probably need to return the ticker and cik for future use...
+	return ""
+}
+
+//Accepts user input about company,checks user input for length
 func main() {
 
 	//request user input about company
@@ -59,42 +131,5 @@ func main() {
 	var companySelection string
 	fmt.Scanln(&companySelection)
 
-	//set length of companySelection to variable companySelection
-	var companySelectionLength = len(companySelection)
-
-	//check if companySelection is between 4 and 25 characters
-	if companySelectionLength >= 4 && companySelectionLength <= 25 {
-		fmt.Print("\nOkay, you want to know about ", companySelection, "\n")
-
-		//get the SEC_API_TOKEN from .env file to use in api call
-		dotenv := goDotEnvVariable("SEC_API_TOKEN")
-
-		//call the sec api to get information about companySelection
-		res, err := http.Get("https://api.sec-api.io/mapping/name/" + companySelection + "?token=" + dotenv)
-
-		//handle the errors
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var c []Company
-
-		unmarshaledBody := json.Unmarshal(body, &c)
-		if unmarshaledBody != nil {
-			fmt.Println(unmarshaledBody)
-			return
-		}
-
-		for _, values := range c {
-			fmt.Printf("\nTicker: %#v\nLocation: %#v\n", values.Ticker, values.Location)
-		}
-	} else {
-		fmt.Println("Your entry must be between 4 and 25 characters long, please try again")
-	}
+	mappingApiNameCaller(companySelection)
 }
